39f7b5a8bf111fb4b6d4cbaf98bda868b3c0a444
OVEGERO <rat4355@gmail.com>
2024-11-06 21:00:59 -0500

    Replace node adapter with cloudflare adapter in configuration and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,19 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
-import node from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare';
+
+// import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
-  output: 'hybrid',
 
-  adapter: node({
-    mode: 'standalone',
-  }),
+  // adapter: node({
+  //   mode: 'standalone',
+  // }),
+  integrations: [mdx(), sitemap()],
+
+  output: 'hybrid',
+  adapter: cloudflare(),
 });
